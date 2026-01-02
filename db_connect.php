6d482aec918275d1bd78f3ab25e3a2a3634e4b7a
Jordan-Kouayep-Mbakop <kouayepmbakopjordan1999@gmail.com>
2026-01-02 15:17:40 -0500

    Add files via upload
    pour connecter la base de donnée sur notre serveur local "xamp''

---
+++ db_connect.php
@@ -0,0 +1,22 @@
+
+<?php
+// Paramètres de connexion
+$host = 'localhost'; // Votre hôte
+$db   = 'database'; // Le nom de votre base de données
+$user = 'root'; // Votre utilisateur
+$pass = ''; // Votre mot de passe
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
